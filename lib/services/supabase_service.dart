abb218c779591a64eb2e8cf472304a0b77f56d14
Fadhilah Aditya Akbar <112860183+Fadhilah25@users.noreply.github.com>
2025-12-29 23:24:47 +0700

    p1

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -2,15 +2,13 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseService {
   // TODO: Replace with your actual Supabase URL and Anon Key
-  static const String supabaseUrl = '';
-  static const String supabaseAnonKey = '';
+  static const String supabaseUrl = 'https://gbvcovhvkbfloffzdtmo.supabase.co';
+  static const String supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdidmNvdmh2a2JmbG9mZnpkdG1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMjA5ODAsImV4cCI6MjA4MjU5Njk4MH0.dG3zDG747SwcE0ZBzi0IWg8kzgYK_J_yHuF8SOWvNzA';
 
   static final SupabaseClient client = Supabase.instance.client;
 
   static Future<void> initialize() async {
-    await Supabase.initialize(
-      url: supabaseUrl,
-      anonKey: supabaseAnonKey,
-    );
+    await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
   }
 }
